4ed6f2f6e9ecdabd3e552e9bc8f3d098a5894bab
wallacevff <wallacevff@hotmail.com>
2017-05-26 19:42:25 -0300

    Trabalho 4 acrescentado

--- trabalhos/trabalho-03/Jogo/controle_colisoes.lua
+++ trabalhos/trabalho-03/Jogo/controle_colisoes.lua
@@ -6,6 +6,21 @@ function colide_wall(objeto)
 	if(objeto.y < 0) then
 		objeto.y = 0
 	end
+	
+	-- Trabalho 04
+	--Nome: end
+	--Propriedade: Semântica
+	--Biding Tine: design
+	--Explicação: definida durante a implementação da linguagem. é uma palavra reservada da linguagem que determina o fim de um bloco
+
+	-- Trabalho 04
+	--Nome: objeto
+	--Propriedade: endereço
+	--Biding Tine: execução
+	--Explicação: sendo uma variavrl local, o seu endereço é definido no momento da execução do programa e seu escopo é limitado ao bloco "colide_wall"
+
+	
+	
 	if(objeto.y  > Height - objeto.height) then
 		objeto.y = Height - objeto.height
 	end
