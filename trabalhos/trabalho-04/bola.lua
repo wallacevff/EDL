a4e1245b6a28c95832ce4f836d2b6765d2561731
wallacevff <wallacevff@hotmail.com>
2017-06-10 12:54:38 -0300

    Create bola.lua

--- trabalhos/trabalho-04/bola.lua
+++ trabalhos/trabalho-04/bola.lua
@@ -1,36 +1,15 @@
---Exemplo de array em Lua (bola = {})
+
 bola = {}
 bola_colide_vertical = false
 bola_colide_horizontal = false
 
 function bola.load()
-	--Tarefa 6:
-		-- Exemplo de enumeracao em Lua
-	if(center_bar_active == false) then -- Verifica se a barra central está ativa
-		bola.x = Width / 2
-		bola.y = Height / 2
-		bola.r = 20
-		bola.speed = 500
-		bola.xvel = bola.speed * math.cos(math.pi / 4)
-		bola.yvel = bola.speed * math.sin(math.pi / 4)
-		
-	elseif(score_left == false) then -- Verifica se a bola vem ao lado direito da barra central
-		bola.x = Width/2 + 5 +bola.r
-		bola.y = Height / 2
-		bola.r = 20
-		bola.speed = 500
-		bola.xvel = bola.speed * math.cos(math.pi / 4)
-		bola.yvel = bola.speed * math.sin(math.pi / 4)
-	
-	elseif(score_left == true) then -- Verifica se a bola vem ao lado esquerdo da barra central
-		bola.x = Width/2 - bola.r
-		bola.y = Height / 2
-		bola.r = 20
-		bola.speed = 500
-		bola.xvel = bola.speed * math.cos(math.pi / 4)
-		bola.yvel = bola.speed * math.sin(math.pi / 4)
-	
-	end
+	bola.x = Width / 2
+	bola.y = Height / 2
+	bola.r = 20
+	bola.speed = 500
+	bola.xvel = bola.speed * math.cos(math.pi / 4)
+	bola.yvel = bola.speed * math.sin(math.pi / 4)
 end
 
 function bola.update(dt)
@@ -57,48 +36,44 @@ function bola.update(dt)
 	end
 		
 	-- Colisao com o player1 
-	if(bola.x - bola.r <= player1.x + player1.width and (bola.y + bola.r >= player1.y and bola.y - bola.r <= player1.y + player1.height)) then -- verifica colisao com o player 1
+	if (bola.x - bola.r <= player1.x + player1.width and (bola.y + bola.r >= player1.y and bola.y - bola.r <= player1.y + player1.height)) then -- verifica colisao com o player 1
 		bola.x = bola.x+bola.r
 		bola_colide_horizontal = false -- colidiu com o player 1
 	end
 	
 	if (bola.x - bola.r < 0) then -- Verifica se a bola saiu pelo lado esquerdo
 		score[4] = score[4] + 1
-		center_bar_update()
 		score_left = true -- se a bola sair pelo lado direito ela vai aparecer no lado direito
 		bola.load()
 		start = false
 	end
 
 	-- Colisao com o player 2
-	if(bola.x + bola.r >= player2.x and bola.y +bola.r >= player2.y and bola.y - bola.r <= player2.y +player2.height) then -- verifica colisao com o player 2
+	if (bola.x + bola.r >= player2.x and bola.y +bola.r >= player2.y and bola.y - bola.r <= player2.y +player2.height) then -- verifica colisao com o player 2
 		bola.x = bola.x - bola.r
 		bola_colide_horizontal = true
 	end
 	
 	if(bola.x +bola.r > Width) then -- verifica se a bola saiu pelo lado direito
 		score[2] = score[2] + 1
-		center_bar_update()
 		score_left = false -- se a bola sair pelo lado esquerdo, ela aparecera no lado esquerdo
 		bola.load()	
 		start = false
 	end
-	
-	if(center_bar_active and score_left == true and start == true) then -- controle de colisao da bola com a barra central
-		if(bola.x + bola.r >= Width/2) then
+
+	for i,bar in ipairs(bars) do -- Verifica colisão da bola com as barras que aparecem
+		if(bola.x - bola.r <= bar.x + bar.width and bola.x - bola.r >= bar.x and bola.y + bola.r >= bar.y and bola.y - bola.r <= bar.y + bar.height and bola_colide_horizontal == true) then
+			bola.x = bola.x + bola.r
+			bola_colide_horizontal = false
+			table.remove(bars,i)
+		end
+		if (bola.x + bola.r >= bar.x and  bola.x + bola.r <= bar.x + bar.width and bola.y + bola.r >= bar.y and bola.y - bola.r <= bar.y + bar.height and bola_colide_horizontal == false) then
 			bola.x = bola.x - bola.r
 			bola_colide_horizontal = true
-			center_bar_collection[1].hit = center_bar_collection[1].hit+1 -- variavel que define a vida util da barra central
+			table.remove(bars,i)
 		end
-	end
-	if(center_bar_active and score_left == false and start == true) then
-		if(bola.x - bola.r <= Width/2 + 5) then
-			bola.x = bola.x+bola.r
-			bola_colide_horizontal = false
-			center_bar_collection[1].hit = center_bar_collection[1].hit+1 -- variavel de determina a vida util da barra central
-		end
-	end	
 	
+	end
 
 	
 end
@@ -108,3 +83,4 @@ end
 function bola.draw()
 	love.graphics.circle("fill", bola.x, bola.y, bola.r)
 end
+
