91ff577bbe5f0050c5bfcc2f39ef2d48667d99f4
wallacevff <wallacevff@hotmail.com>
2017-06-10 12:56:29 -0300

    Tarefa 06 e 07 (correção)

--- trabalhos/trabalho-04/main.lua
+++ trabalhos/trabalho-04/main.lua
@@ -2,19 +2,15 @@ require("player1")
 require("player2")
 require("bola")
 require("controle_colisoes")
-require("center_bar")
+require("bars")
 start = false
-score_left = false
-
 --Tarefa: 6
 	--Exemplo de Tupla em Lua (Score)
 score = {"Score Player 1:   ", 0, "Score Player 2:   ", 0}
 
 function love.load()
 	player1.load()
-	--center_bar_load()
 	bola.load()
-	--table.insert(center_bar_collection, center_bar)
 	player2.load()
 end
 
@@ -23,17 +19,26 @@ function love.update(dt)
 	colide_wall(player1)
 	player2.update(dt)
 	colide_wall(player2)
+--Tarefa 06
+	--Exemplo de enumeração em Lua
+	if love.keyboard.isDown("r") then  -- deleta todos os itens do array bars
+		start = false
+		player1.load()
+		bola.load()
+		player2.load()
+		score[2] = 0
+		score[4] = 0
+		for bar,i in ipairs(bars) do
+			table.remove(bars,bar)
+		end
+	end
+	-------------------------------
 	if(start == true) then
 		bola.update(dt)
-	--Tarefa 06:
-	--Exemplo de enumeração em Lua:
+		bars.update(dt)
 	elseif love.keyboard.isDown("space") then
 		bola.update(dt)
 		start = true
-	end
-	if(center_bar_collection[1] and center_bar_collection[1].hit == 3) then
-		center_bar_update()
-		center_bar_load()
 	end
 		
 end
@@ -42,7 +47,7 @@ function love.draw()
 	player1.draw()
 	player2.draw()
 	bola.draw()
-	center_bar_draw()
 	love.graphics.print(score[1] .. score[2],160,16)
 	love.graphics.print(score[3] .. score[4],700,16)
+	bars_draw()
 end
